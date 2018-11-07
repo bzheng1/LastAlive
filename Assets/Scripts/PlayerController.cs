ae1ea9671563dbbe0717d3d902ff9d89efcda909
Bzheng <bzheng@uno.edu>
2018-11-07 02:29:37 -0600

    first commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerController : MonoBehaviour {
+    Rigidbody myrigidbody;
+    Vector3 velocity;
+
+	// Use this for initialization
+	void Start () {
+        myrigidbody = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void Move(Vector3 _velocity)
+    {
+        velocity = _velocity;
+    }
+
+    public void FixedUpdate()
+    {
+        myrigidbody.MovePosition(myrigidbody.position + velocity * Time.fixedDeltaTime);
+    }
+}
